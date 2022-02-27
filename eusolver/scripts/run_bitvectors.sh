11820d4993d2da9f800f92cea1cde5984570717c
Megan Chu <megan.chu2@gmail.com>
2022-02-27 01:16:19 +0000

    update PBE strings and BV scripts

--- eusolver/scripts/run_bitvectors.sh
+++ eusolver/scripts/run_bitvectors.sh
@@ -4,10 +4,18 @@ trap "exit" INT
 
 rm -f log_bitvectors
 for file in benchmarks-master/comp/2018/PBE_BV_Track/*.sl; do
+	echo "===================="
 	echo "====================" >> log_bitvectors
+	echo "START BENCHMARK: $file"
 	echo "START BENCHMARK: $file" >> log_bitvectors
-	#PYTHONPATH=$PYTHONPATH:../thirdparty/libeusolver/build/:../thirdparty/z3/build/python:../thirdparty/bitstring-3.1.3/ timeout "$1" python3 core/solvers.py icfp $file log
-	timeout 30m time ./eusolver "$file" >> log_bitvectors
+	#PYTHONPATH=$PYTHONPATH:../thirdparty/libeusolver/build/:../thirdparty/z3/build/python:../thirdparty/bitstring-3.1.3/ timeout "$1" python3 core/solvers.py icfp $file log:wq
+	start=`date +%s%N`
+	timeout 30m ./eusolver "$file" >> log_bitvectors
+        end=`date +%s%N`
+	echo `expr $end - $start`
+	echo `expr $end - $start` >> log_bitvectors
+	echo "END BENCHMARK: $file"
 	echo "END BENCHMARK: $file" >> log_bitvectors
+	echo "===================="
 	echo "====================" >> log_bitvectors
 done
--- eusolver/scripts/run_strings.sh
+++ eusolver/scripts/run_strings.sh
@@ -4,10 +4,18 @@ trap "exit" INT
 
 rm -f log_strings
 for file in benchmarks-master/comp/2018/PBE_Strings_Track/*.sl; do
+	echo "===================="
 	echo "====================" >> log_strings
+	echo "START BENCHMARK: $file"
 	echo "START BENCHMARK: $file" >> log_strings
-	#PYTHONPATH=$PYTHONPATH:../thirdparty/libeusolver/build/:../thirdparty/z3/build/python:../thirdparty/bitstring-3.1.3/ timeout "$1" python3 core/solvers.py icfp $file log
-	timeout 30m time ./eusolver "$file" >> log_strings
+	#PYTHONPATH=$PYTHONPATH:../thirdparty/libeusolver/build/:../thirdparty/z3/build/python:../thirdparty/bitstring-3.1.3/ timeout "$1" python3 core/solvers.py icfp $file log:wq
+	start=`date +%s%N`
+	timeout 30m ./eusolver "$file" >> log_strings
+        end=`date +%s%N`
+	echo `expr $end - $start`
+	echo `expr $end - $start` >> log_strings
+	echo "END BENCHMARK: $file"
 	echo "END BENCHMARK: $file" >> log_strings
+	echo "===================="
 	echo "====================" >> log_strings
 done
