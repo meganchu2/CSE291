06f0e932548e0f812126af4b2c949b6dec43bc58
klivoth <31941554+klivoth@users.noreply.github.com>
2022-03-02 15:34:28 -0800

    Print function for AST

--- gpsolver/src/program.py
+++ gpsolver/src/program.py
@@ -8,7 +8,6 @@ class Production:
         self.lhs = lhs
         self.rhs = rhs
 
-
 class Node:
     def __init__(self, expr_type: str) -> None:
         self.parent = None
@@ -25,7 +24,7 @@ class Node:
     def to_dict(self) -> dict[str, Any]:
         return {"node_type": self.node_type, "expr_type": self.expr_type, "depth": self.depth, "id": self.id}
 
-    def apply(self, rule: grammar.Production) -> Node:
+    def apply(self, rule: Production) -> Node:
         assert self.__eq__(rule.lhs)
         new = deepcopy(rule.rhs)
         new.id = self.id
@@ -40,6 +39,9 @@ class Node:
             self.parent.children[int(self.id[-1])] = new
         return new
 
+    def get_name(self) -> str:
+        return ""
+
 class VarNode(Node):
     def __init__(self, var_name: str, expr_type: str) -> None:
         super().__init__(expr_type)
@@ -49,10 +51,16 @@ class VarNode(Node):
     def __eq__(self, o: object) -> bool:
         return super().__eq__(o) and isinstance(o, VarNode) and self.name == o.name
 
+    def __hash__(self) -> int:
+        return hash(str(self.to_dict()))
+
     def to_dict(self) -> dict[str, Any]:
         d = super().to_dict()
         d["name"] = self.name
         return d
+
+    def get_name(self) -> str:
+        return self.name
 
 class ConstNode(Node):
     def __init__(self, expr_type: str, value: Any) -> None:
@@ -63,10 +71,16 @@ class ConstNode(Node):
     def __eq__(self, o: object) -> bool:
         return super().__eq__(o) and isinstance(o, ConstNode) and self.value == o.value
 
+    def __hash__(self) -> int:
+        return hash(str(self.to_dict()))
+
     def to_dict(self) -> dict[str, str]:
         d = super().to_dict()
         d["value"] = self.value
         return d
+
+    def get_name(self) -> str:
+        return str(self.value)
 
 class FuncNode(Node):
     def __init__(self, func_name: str, expr_type: str) -> None:
@@ -77,10 +91,16 @@ class FuncNode(Node):
     def __eq__(self, o: object) -> bool:
         return super().__eq__(o) and isinstance(o, FuncNode) and self.func_name == o.func_name
 
+    def __hash__(self) -> int:
+        return hash(str(self.to_dict()))
+
     def to_dict(self) -> dict[str, str]:
         d = super().to_dict()
         d["func_name"] = self.func_name
         return d
+
+    def get_name(self) -> str:
+        return self.func_name
 
 class NTNode(Node): # non-terminal
     def __init__(self, nt_name: str, expr_type: str) -> None:
@@ -91,10 +111,16 @@ class NTNode(Node): # non-terminal
     def __eq__(self, o: object) -> bool:
         return super().__eq__(o) and isinstance(o, NTNode) and self.nt_name == o.nt_name
 
+    def __hash__(self) -> int:
+        return hash(str(self.to_dict()))
+
     def to_dict(self) -> dict[str, str]:
         d = super().to_dict()
         d["nt_name"] = self.nt_name
         return d
+
+    def get_name(self) -> str:
+        return self.nt_name
 
 def is_terminal(n: Node) -> bool:
     if isinstance(n, NTNode):
@@ -102,3 +128,17 @@ def is_terminal(n: Node) -> bool:
     if isinstance(n, FuncNode):
         return all([is_terminal(c) for c in n.children])
     return True
+
+def print_ast(n: Node) -> str:
+    if isinstance(n, ConstNode):
+        if n.value == "":
+            return "\'\'"
+        if n.value == " ":
+            return "\' \'"
+        return str(n.value)
+    if isinstance(n, FuncNode):
+        s = n.get_name() + "("
+        s += ", ".join(print_ast(child) for child in n.children)
+        s += ")"
+        return s
+    return n.get_name()
