5bec6f53bdcf0d0a069857559175410af31f13fe
Mayank Sharan <mayanksharan96@gmail.com>
2022-03-03 22:25:03 -0800

    adding levenshtien distance

--- gpsolver/src/gp.py
+++ gpsolver/src/gp.py
@@ -1,3 +1,33 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@sharanmayank 
+meganchu2
+/
+CSE291
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+1
+Wiki
+Security
+Insights
+CSE291/gpsolver/src/gp.py /
+@klivoth
+klivoth Fitness function in lexicase selection
+Latest commit 74b5205 22 minutes ago
+ History
+ 3 contributors
+@meganchu2@klivoth@sharanmayank
+451 lines (325 sloc)  14.1 KB
+   
 import copy
 import random
 import numpy as np
@@ -416,6 +446,48 @@ def load_examples(benchmark_file):
 
     return eg_info_dict
 
+
+def printDistances(distances, token1Length, token2Length):
+
+    for t1 in range(token1Length + 1):
+        for t2 in range(token2Length + 1):
+            print(int(distances[t1][t2]), end=" ")
+        print()
+
+
+def levenshteinDistanceDP(token1, token2):
+
+    distances = np.zeros((len(token1) + 1, len(token2) + 1))
+
+    for t1 in range(len(token1) + 1):
+        distances[t1][0] = t1
+
+    for t2 in range(len(token2) + 1):
+        distances[0][t2] = t2
+        
+    a = 0
+    b = 0
+    c = 0
+    
+    for t1 in range(1, len(token1) + 1):
+        for t2 in range(1, len(token2) + 1):
+            if (token1[t1-1] == token2[t2-1]):
+                distances[t1][t2] = distances[t1 - 1][t2 - 1]
+            else:
+                a = distances[t1][t2 - 1]
+                b = distances[t1 - 1][t2]
+                c = distances[t1 - 1][t2 - 1]
+                
+                if (a <= b and a <= c):
+                    distances[t1][t2] = a + 1
+                elif (b <= a and b <= c):
+                    distances[t1][t2] = b + 1
+                else:
+                    distances[t1][t2] = c + 1
+
+    return distances[len(token1)][len(token2)]
+
+
 if __name__ == '__main__':
 
 
@@ -448,4 +520,3 @@ if __name__ == '__main__':
         print("Solution size", prog_size(solution))
     else:
         print("Unable to find a solution")
-
